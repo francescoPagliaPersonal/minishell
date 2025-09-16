3dace2ba5d9eda0767426c01b4dca1044ddb2aa4
fpaglia <fpaglia@student.42vienna.com>
2025-09-16 18:51:53 +0200

    simple example to analize the dup2 behavior

--- playg-fra/fd_shifting.c
+++ playg-fra/fd_shifting.c
@@ -6,30 +6,51 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 14:00:24 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/09/16 14:24:35 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/09/16 15:52:07 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "../libs/libft/libft.h"
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	int fd;
-	char buff[200];
+    int		fdin;
+	int 	fdout;
 
-	fd = open("permute", O_WRONLY | O_CREAT | O_APPEND, 0644);
-	dup2(fd, STDOUT_FILENO);
-	printf("the new fd is: %d  ", fd);
-	while (*buff != '~')
+    char    *str;
+	char	*a = "a";
+	
+	/*some preliminary ideas to get the username on the shell together 
+	 * with the current path */
+	printf("%s::%s\n %%", getenv("USER"), getenv("PWD"));
+
+	fdin = 0;
+	/* created a new file that uses the following bitmasks for the options:
+	 * O_WRONLY: open with the purpose to only write into it
+	 * O_CREAT: create the file shall it not exists
+	 * 0_APPEND: start from the last line do not allow for data overwrite.*/
+	fdout = open("permute", O_WRONLY | O_CREAT | O_APPEND, 0644);
+	/* mapped the new file to the stdout so the data is written there*/
+	dup2(fdout, STDOUT_FILENO);
+	str = a;
+	while (str[0] != '~')
 	{
-		read(0, buff, 128);
-		printf("%s", buff);
+		str = get_next_line(fdin);
+		/*open the file defined above with tail -f permute notice at any new line
+		 * the data is written in there*/
+		printf("%s\n", str);
+		if (str[0] != '~')
+		{
+	    	free(str);
+			str = a;
+		}
 	}
-//	printf("|  I'm writing something to the new file!\n");
-	close(fd);
+	free(str);
 	return (0);
 
 }
+
