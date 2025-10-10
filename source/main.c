7e06886c7b59810b32e2510a7be5ca90abbd4e55
Vesta Manuyko <Vesta.manuyko@gmail.com>
2025-10-10 16:27:37 +0200

    add: sigaction and a simple handler

--- source/main.c
+++ source/main.c
@@ -12,11 +12,23 @@
 
 #include <minishell.h>
 
+void	handler(int	signum)
+{
+	(void)signum;
+	write(1, "Received Signal\n", 16);
+}
+
 int main(int ac, char **av, char **env)
 {
 	t_shell	shell;
+	struct sigaction	sa;
 
 	init_shell(&shell);
+	sa.sa_sigaction = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 	while (1)
     {
     	if (get_command(&shell))
@@ -24,8 +36,8 @@ int main(int ac, char **av, char **env)
         	populate_programs(&shell);
         	if (validate_programs(&shell))
 			{
-			run_programs(&shell);
-			free_programs(&shell)
+				run_programs(&shell);
+				free_programs(&shell);
 			}
 		}
     }
